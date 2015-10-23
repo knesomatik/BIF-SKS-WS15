153ab5852030fb5c1ce50ae7df602cb748917d57
Felix Klein <me@felixklein.at>
2015-10-23 13:03:51 +0200

    i forgot

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -18,34 +18,37 @@ RestartSec=30
 WantedBy=multi-user.target
 '
 
-# update system
-yum -y update
+(
+	# update system
+	yum -y update
 
-# install java
-yum -y install java-1.8.0-openjdk
+	# install java
+	yum -y install java-1.8.0-openjdk
 
-# set wildfly variables
-export WILDFLY_VERSION="9.0.1.Final"
-export WILDFLY_SHA1="abe037d5d1cb97b4d07fbfe59b6a1345a39a9ae5"
-export JBOSS_HOME="/opt/jboss/wildfly"
+	# set wildfly variables
+	export WILDFLY_VERSION="10.0.0.CR3"
+	export WILDFLY_SHA1="c14453890657e05fae0e2765c9dca8a35b2b2096"
+	export JBOSS_HOME="/opt/jboss/wildfly"
 
-# create dir
-mkdir -p $JBOSS_HOME
+	# create dir
+	mkdir -p $JBOSS_HOME
 
-# download, verify and install wildfly
-cd $HOME \
+	# download, verify and install wildfly
+	cd $HOME \
 	&& curl -O https://download.jboss.org/wildfly/$WILDFLY_VERSION/wildfly-$WILDFLY_VERSION.tar.gz \
 	&& sha1sum wildfly-$WILDFLY_VERSION.tar.gz | grep $WILDFLY_SHA1 \
 	&& tar xf wildfly-$WILDFLY_VERSION.tar.gz \
 	&& mv $HOME/wildfly-$WILDFLY_VERSION/* $JBOSS_HOME/ \
 	&& rm wildfly-$WILDFLY_VERSION.tar.gz
 
-# add user to wildfly
-/opt/jboss/wildfly/bin/add-user.sh -u x -p x
+	# add user to wildfly
+	/opt/jboss/wildfly/bin/add-user.sh -u x -p x
 
-# install and enable wildfly systemd service
-echo "$wildfly_startup" > /usr/lib/systemd/system/wildfly.service
-systemctl enable wildfly.service
-systemctl start wildfly.service
+	# install and enable wildfly systemd service
+	echo "$wildfly_startup" > /usr/lib/systemd/system/wildfly.service
+	systemctl enable wildfly.service
+	systemctl start wildfly.service
+
+) | tee ~/setup.log
 
 exit 0
