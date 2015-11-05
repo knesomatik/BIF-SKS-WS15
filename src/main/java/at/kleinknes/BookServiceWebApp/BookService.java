c1d2188cf0e9e3d88444309d9652d1dc34de6390
Felix Klein <me@felixklein.at>
2015-11-05 23:58:43 +0100

    do some cleanup stuff

--- src/main/java/at/kleinknes/BookServiceWebApp/BookService.java
+++ src/main/java/at/kleinknes/BookServiceWebApp/BookService.java
@@ -14,18 +14,13 @@ public class BookService {
 	@PersistenceContext
 	private EntityManager em;
 
-	public void removeNews(Long id) {
-		Book newS = em.find(Book.class, id);
-		em.remove(newS);
-	}
-
 	public boolean saveBooks(Object book) {
 
 		List<Book> books;
 
 		if (book instanceof Book) {
 			// we have only on book
-			books = new ArrayList<Book>();
+			books = new ArrayList<>();
 			books.add((Book) book);
 		} else if (book instanceof List) {
 			// we have a list of books
@@ -36,25 +31,22 @@ public class BookService {
 		}
 
 		for (Book n : books) {
-			assert n.getTitle() != null;
-			assert n.getDate() != null;
+			if (n.getTitle() == null) return false;
+			if (n.getDate() == null) return false;
 		}
 
 
 		Session session = (Session) em.getDelegate();
-		Transaction tx = null;
-		tx = session.getTransaction();
-		tx.begin();
+		Transaction tx = session.beginTransaction();
 
 		try {
 			session.persist(book);
-			tx.commit();
 		} catch (Exception e) {
-			if (tx != null) {
-				tx.rollback();
-			}
+			tx.rollback();
 			return false;
 		}
+
+		tx.commit();
 
 		return true;
 	}
@@ -67,16 +59,5 @@ public class BookService {
 	public List<Book> searchBooks(String title) {
 		return em.createNamedQuery("Book.searchAll", Book.class).setParameter("search", "%" + title + "%").getResultList();
 	}
-
-	public void alterText(String first) {
-		List<Book> list = em.createNamedQuery("Book.selectAll", Book.class).getResultList();
-
-		int count = 0;
-		for (Book n : list) {
-			n.setTitle(first + count);
-			count++;
-		}
-	}
-
 
 }
